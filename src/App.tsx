6962d3a256d56c09766a22b627e7c1f2af656d9f
4kytwatchhourstime <famousfivecoin@gmail.com>
2026-02-25 18:10:53 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OptInPage from './OptInPage';
 import SalesPage from './SalesPage';
+import ThankYouPage from './ThankYouPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<OptInPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
         <Route path="/sales" element={<SalesPage />} />
         {/* Fallback to Opt-in */}
         <Route path="*" element={<OptInPage />} />
