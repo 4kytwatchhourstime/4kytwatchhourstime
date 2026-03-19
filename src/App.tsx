19c9aace9470d5ae0dc9325f7da6eaaac235bf97
4kytwatchhourstime <famousfivecoin@gmail.com>
2026-03-19 16:04:18 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import OptInPage from './OptInPage';
 import SalesPage from './SalesPage';
 import ThankYouPage from './ThankYouPage';
 import About from './AboutPage';
-import About from './JVSalesPage';
+import About from './JVSales';
 
 export default function App() {
   return (
@@ -19,7 +19,7 @@ export default function App() {
         <Route path="/opt-in" element={<OptInPage />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
         <Route path="/about" element={<About />} />
-        <Route path="/jv" element={<JVSalesPage />} />
+        <Route path="/jv" element={<JVSales />} />
         {/* Fallback to Sales Page */}
         <Route path="*" element={<SalesPage />} />
       </Routes>
