bfd82f384c11eeabcc37683112d31e64d8d0928c
4kytwatchhourstime <famousfivecoin@gmail.com>
2026-03-20 18:10:12 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import SalesPage from './SalesPage';
 import ThankYouPage from './ThankYouPage';
 import About from './AboutPage';
 import JVSalesPage from './JVSalesPage';
+import ArticlePage from './ArticlePage';
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/thank-you" element={<ThankYouPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/jvzoo" element={<JVSalesPage />} />
+        <Route path="/how-to-get-4000-watch-hours" element={<ArticlePage />} />
         {/* Fallback to Sales Page */}
         <Route path="*" element={<SalesPage />} />
       </Routes>
