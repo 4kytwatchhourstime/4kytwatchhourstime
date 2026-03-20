a5b7cc8d87258cf2243fb7eece2f3c401700236d
4kytwatchhourstime <famousfivecoin@gmail.com>
2026-03-21 03:00:26 +0800

    Update AboutPage.tsx

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -89,7 +89,7 @@ export default function About() {
         >
           <h3 className="text-3xl font-black tracking-tight">CONTACT <span className="text-[#FFD700]">ME</span></h3>
           <p className="text-gray-400 text-lg max-w-3xl mx-auto leading-relaxed">
-            I saw too many talented creators — including myself — give up right before the finish line. The “4,000 watch hours wall” is where most dreams come to a stop. I created this blueprint to make sure your voice gets heard and your hard work gets the results it deserves. If you want updates, new strategies, or improvements to this system… You can reach me here: <a className="text-[#FFD700]" href="mailto:4kytwatchhours@gmail.com">Use Right Click Here to Contact Me</a>
+            I saw too many talented creators — including myself — give up right before the finish line. The “4,000 watch hours wall” is where most dreams come to a stop. I created this blueprint to make sure your voice gets heard and your hard work gets the results it deserves. If you want updates, new strategies, or improvements to this system… You can reach me here: <a className="text-[#FFD700]" href="mailto:blogginggurucom@gmail.com">Use Right Click Here to Contact Me</a>
           </p>
         </motion.div>
         
